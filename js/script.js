fd4c8c8a8bac8d6b4d76ce2413dfedcb0d4890ce
shaunakmcdonald <shaunamcdonald@Shaunas-MacBook-Pro.local>
2016-07-07 17:32:04 +1000

    Assignment 6
    Working with Arrays

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var cities = [["NYC", 'nyc'], ["SF", 'sf'], ["LA", 'la'], ["ATX", 'austin'], ["SYD", 'syd']];
+
+	// for (var i=0; i<cities.length; i++){
+	$(cities).each(function(index, element){
+		$('#city-type').append("<option value='"+element[1]+"'>" + element[0] + "</option>");
+	});
+	
+
+	$('#city-type').change(function(){
+		$('body').attr('class', $(this).val());
+	});
+});
